d9d6791a368500afccc57a0ea57a337ae5d6f875
moyasiginko <mrsiyam61@gmail.com>
2025-04-03 16:23:57 +0600

    hero section setup

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -31,14 +31,14 @@ const Hero = () => {
        *  0.2 to 0.03
        */}
       <div
-        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+        className="h-screen w-full dark:bg-slate-950 bg-black dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
       >
         {/* Radial gradient for the container to give a faded look */}
         <div
           // chnage the bg to bg-black-100, so it matches the bg color and will blend in
           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
-         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+         bg-blue-950 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
       </div>
 
@@ -69,6 +69,7 @@ const Hero = () => {
               position="right"
             />
           </a>
+          {/* <ThemeToggler /> */}
         </div>
       </div>
     </div>
