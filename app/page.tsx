ed50878629f6d8aab6cffb0aa98728ee4cb59389
moyasiginko <mrsiyam61@gmail.com>
2025-04-04 00:24:06 +0600

    setup recent projects

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import Image from "next/image";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grids";
+import RecentProjects from "@/components/Projects";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <Header />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
