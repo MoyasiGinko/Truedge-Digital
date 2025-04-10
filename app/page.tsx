fcb2b5af10d8296714f6eaf42683aba67e9b9eb6
moyasiginko <mrsiyam61@gmail.com>
2025-04-10 07:28:03 +0600

    fix the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ import ContactSection from "@/components/Contact";
 export default function Home() {
   return (
     <>
+      <Hero />
       <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-        <Hero />
         <div className="max-w-7xl w-full">
           <Grid />
           <RecentProjects />
