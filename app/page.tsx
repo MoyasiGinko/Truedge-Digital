a998e620da93866e0d239347bf1fc10f0103ac70
moyasiginko <mrsiyam61@gmail.com>
2025-04-10 06:23:59 +0600

    setup  contact page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Clients from "@/components/Clients";
 // import Experience from "@/components/Experience";
 // import Approach from "@/components/Approach";
 import CombinedSections from "@/components/CombinedSections";
+import ContactSection from "@/components/Contact";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         </div>
       </main>
       <CombinedSections />
+      <ContactSection />
     </>
   );
 }
